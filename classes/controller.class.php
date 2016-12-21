5d6c8da07d9df053db941168a68ffe37041a4ae5
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-12-21 03:49:16 -0500

    forgot to push these files, should be good now

---
+++ classes/controller.class.php
@@ -0,0 +1,17 @@
+<?php
+    abstract class controller {
+      
+      protected $html;
+      public function get() {}
+      public function post() {}
+      public function put() {}
+      public function delete() {}
+      public function __construct() {
+        $header = 'Home';
+	$this->html .= $header;
+      }
+      public function getHTML() {
+        
+	return $this->html;
+      }
+    }
